76d3d20dae1edb846c361c1653ba56c7063bb791
Connor Bell <ronnoclleb@gmail.com>
2024-04-05 13:06:48 -0400

    added carousel

--- frontend/raku-tracker/app/components/Carousel.js
+++ frontend/raku-tracker/app/components/Carousel.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Carousel.css';
+import Drawing from "@/app/components/Drawing";
 
 const Carousel = props => {
     // carousel of random images
     return (
         <div className="carousel">
-
+            <Drawing />
+            <Drawing />
+            <Drawing />
+            <Drawing />
+            <Drawing />
         </div>
     );
 };
