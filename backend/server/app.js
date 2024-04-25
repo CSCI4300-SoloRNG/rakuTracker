456aaa857556674e5e6adcb0f599f13b9d72c754
Connor Bell <ronnoclleb@gmail.com>
2024-04-25 19:45:44 -0400

    oops

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 42069
 const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv').config();
 
 app.use(cors({origin: true, credentials: true }));
 const drawings = require('./routes/api/drawing');
 app.use('/s', drawings);
 
-const conn_str = 'mongodb+srv://adw13800:1aGyF6zRuTW21LND@cluster0.77unkre.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const conn_str = process.env.CONN_STR;
 mongoose.set('strictQuery', false);
 mongoose.connect(conn_str).then(() => {
-  app.listen(port, () => console.log('Server running on port ${port}'));
+  app.listen(port, () => console.log(`Server running on port ${port}`));
   console.log("MongoDB Connection Suceeded...");  
 })
 .catch(err=> {
