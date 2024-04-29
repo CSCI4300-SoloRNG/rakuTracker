c4e526906f68caae299a557fd4b914167b0f8c86
Connor Bell <ronnoclleb@gmail.com>
2024-04-29 15:31:59 -0400

    create account working

--- frontend/raku-tracker/app/components/BackendInterface.js
+++ frontend/raku-tracker/app/components/BackendInterface.js
@@ -127,20 +127,25 @@ export async function authenticate(username, password) {
 }
 
 export async function createAccount(email, username, password) {
-    let response;
-    try {
-        // TODO send username and password to server
-        // TODO check response from server.
+    // TODO send username and password to server
+    // TODO check response from server.
 
-        // TODO this is placeholder!!!
-        response = {"ok": true};
-    } catch (e) {
-        // if error, catch and return false.
-        console.log(e);
+
+    const response = await axios.post("http://localhost:42069/api/user/signup", {
+        "email": email,
+        "username": username,
+        "password": password
+    }, {
+        headers: {
+            "Content-Type": "application/json; charset=utf-8"
+        }
+    }).catch(error => {
+        console.log(error);
         return false;
-    }
+    });
+    console.log(response);
 
-    return response.ok;
+    return response.status===200;
 }
 
 export async function isLoggedIn() {
