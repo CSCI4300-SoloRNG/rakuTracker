6273349602ffe51af5ebd9b24b7dcc35ab291b82
flashcap777 <flashcapsonicfan@gmail.com>
2024-04-29 16:42:18 -0400

    cookieJwt has a route now, please test with real token
    works and rejects malformed and invalid tokens. need to test with real token

--- backend/server/routes/api/cookieJwtAuth.js
+++ backend/server/routes/api/cookieJwtAuth.js
@@ -2,16 +2,19 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require("body-parser");
 const jwt = require('jsonwebtoken');
-module.exports = router;
 
-router.cookieJwtAuth = (req, res, next) => {
-    const token = req.cookies.token;
+module.exports = router;
+router.post('/', bodyParser.json(), (req, res) => {
+    const token = req.body.token;
     try {
         const user = jwt.verify(token, process.env.MY_SECRET);
-        req.user = user;
+        res.status(200).json({user});
+        console.log("Token success!")
         next();
     } catch (err) {
         res.clearCookie("token");
+        res.status(403).json({error: "Error validating token: " + err});
+        console.log("Token Error " + err);
         return res.redirect("/");
     }
-};
+});
