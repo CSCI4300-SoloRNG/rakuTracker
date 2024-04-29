8ec89c2f9dce77f225540baae626c9a36ea1ba29
flashcap777 <flashcapsonicfan@gmail.com>
2024-04-29 01:30:09 -0400

    added authentication
    authentication returns either a session token or error message on invalid login

--- backend/server/routes/api/login.js
+++ backend/server/routes/api/login.js
@@ -9,17 +9,21 @@ module.exports = router;
 SECRET = "baka"
 
 
-const validate = async (req, res) => 
+const validate = async (req) => 
+{
+
+}
+
+router.post('/', bodyParser.json(), (req, res) =>  
 {
      // Using find method to get user by username
-    User.findOne({
+     User.findOne({
         username: req.body.username
     })
     .then(user => {
         if (!user) {
             console.log('User not found');
             return;
-            
         }
         // Accessing the found user
         console.log('Found user:', user);
@@ -27,26 +31,17 @@ const validate = async (req, res) =>
         console.log('Email:', user.email);
         const { username, password, email } = user;
         console.log(username + " " + password)
-        bcrypt.compare(req.body.password, password, function(err, res) {
-            if (res) {
-            // Passwords match
+        bcrypt.compare(req.body.password, password, function(_err, _res) {
+            if (_res) {
             delete user.password
             var token = jwt.sign({user}, SECRET, { expiresIn: '1h' })
-            res.cookie("token", token, {
-                httpOnly: true
-            })
-            return res.status(200)
+            res.json({token: token})
             } else {
-            console.log(username)
+            res.status (403).json({ error: 'Invalid username or password' })
             }
             });
     })
     .catch(err => {
         console.error('Error finding user:', err);
     });
-}
-
-router.post('/', bodyParser.json(), (req, res) =>  
-{
-    validate(req, res)
 })
