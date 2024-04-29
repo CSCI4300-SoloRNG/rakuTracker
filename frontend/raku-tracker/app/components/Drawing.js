fe3c9e7600845128d7b68fe222373f0ef6f242d0
Connor Bell <ronnoclleb@gmail.com>
2024-04-28 21:42:09 -0400

    drawings redirect to edit page on click, and have gray placeholder images now

--- frontend/raku-tracker/app/components/Drawing.js
+++ frontend/raku-tracker/app/components/Drawing.js
@@ -1,14 +1,19 @@
+"use client"
 import React from 'react';
 import './Drawing.css';
 
 const Drawing = props => {
     // props.img props.prompt props.tags prompts.time
     return (
-        <div className="drawing">
-            <img
-                src={props.img ? props.img :
-                    "https://cdn.discordapp.com/attachments/1008460812710596668/1233141115100860500/rakutrackerSplash.png?ex=662c03a8&is=662ab228&hm=70ab7a90846afd238cf200fc7f0cf2e4c9865aa766e1a4c99f9091d9a996ad29&"}
-                alt="placeholder image"/>
+        <div className="drawing" onClick={()=>location.assign(`/drawing/edit/${props.id}`)}>
+            {props.img && props.img !== ""? (
+                <img
+                    src={props.img}
+                    alt="your drawing"
+                />
+            ) : (
+                <div className="fake-img"></div>
+            )}
         </div>
     );
 };
