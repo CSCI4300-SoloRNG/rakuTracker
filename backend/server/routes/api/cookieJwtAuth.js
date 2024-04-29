f857e01aa9fafbc2150bfa0921f29105d9aca623
Connor Bell <ronnoclleb@gmail.com>
2024-04-29 16:44:31 -0400

    cookie error logging

--- backend/server/routes/api/cookieJwtAuth.js
+++ backend/server/routes/api/cookieJwtAuth.js
@@ -5,12 +5,14 @@ const jwt = require('jsonwebtoken');
 module.exports = router;
 
 router.cookieJwtAuth = (req, res, next) => {
+    console.log("cookieJwtAuth");
     const token = req.cookies.token;
     try {
         const user = jwt.verify(token, process.env.MY_SECRET);
         req.user = user;
         next();
     } catch (err) {
+        console.log(err);
         res.clearCookie("token");
         return res.redirect("/");
     }
