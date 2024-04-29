da15235a0e379d4afde97915dab03988748b4e6b
Connor Bell <ronnoclleb@gmail.com>
2024-04-29 15:09:39 -0400

    fix carousel and drawing css

--- frontend/raku-tracker/app/components/Drawing.js
+++ frontend/raku-tracker/app/components/Drawing.js
@@ -4,19 +4,14 @@ import './Drawing.css';
 
 const Drawing = props => {
     // props.img props.prompt props.tags prompts.time
-    return (
-        <div className="drawing" onClick={()=>location.assign(`/drawing/edit/${props.id}`)}>
-            {props.img && props.img !== ""? (
-                <img
-                    src={props.img}
-                    alt="your drawing"
-                />
-            ) : (
-                <img
-                src={"https://lh3.googleusercontent.com/drive-viewer/AKGpihYP_KEg2Tbfjo5ZefSLuwWh9XPUtmyXHAvrE5Bu2SNPIkoOprCHhPX0SUWrXK2dUzH5EH_0jH288tXucx13b7AsrjirCDOE_Q=s1600-rw-v1"}
-                alt="your drawing"
-                />
-            )}
+    return props.img && props.img !== "" ? (
+        <div className="drawing" onClick={() => location.assign(`/drawing/edit/${props.id}`)}><img
+            src={props.img}
+            alt="your drawing"
+        /></div>
+    ) : (
+        <div className="drawing">
+            <div className="fake-img"></div>
         </div>
     );
 };
